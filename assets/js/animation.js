d0971f6ef2ebce76de10a9c0769dd51cdd96a6f9
Jeremy98-alt <52446231+Jeremy98-alt@users.noreply.github.com>
2021-08-01 13:34:35 +0200

    adding sessionStorage

--- assets/js/animation.js
+++ assets/js/animation.js
@@ -5,56 +5,64 @@
         if($('.btn-primary').text() == "Submit") { // check if it means to register or log-in in the userPage
             var email = $("#exampleInputEmail1").val();
             var password = $("#exampleInputPassword1").val();
+
+            var data = {
+                email : email,
+                password : password
+            };
+      
+            $.ajax({
+                type: "POST",
+                url : "https://07jol9qqrc.execute-api.us-east-1.amazonaws.com/Dev/users",
+                dataType: "json",
+                crossDomain: "true",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(data),
+    
+                success: function () {
+                    // clear form and show a success message
+                    alert("Log-in Success");
+
+                    // save the session in js
+                    sessionStorage.setItem('email', email);
+            
+                    var url = "https://s3.amazonaws.com/prodigit2.0/pages/userPage.html";
+                    $(location).attr('href',url);
+                },
+                
+                error: function () {
+                    // show an error message
+                    alert("UnSuccessfull");
+            }});
         } else {
             var email = $("#exampleInputEmail2").val();
             var password = $("#exampleInputPassword2").val();
+
+            var data = {
+                email : email,
+                password : password
+            };
+      
+            $.ajax({
+                type: "POST",
+                url : "https://07jol9qqrc.execute-api.us-east-1.amazonaws.com/Dev/users",
+                dataType: "json",
+                crossDomain: "true",
+                contentType: "application/json; charset=utf-8",
+                data: JSON.stringify(data),
+    
+                success: function (e) {
+                    // clear form and show a success message
+                    alert(e);
+                    document.getElementById("resizedForm").reset();
+                    location.reload(); 
+                },
+                
+                error: function () {
+                    // show an error message
+                    alert("UnSuccessfull");
+            }});
         }
-
-        var data = {
-            email : email,
-            password : password
-        };
-  
-        $.ajax({
-            type: "POST",
-            url : "https://07jol9qqrc.execute-api.us-east-1.amazonaws.com/Dev/users",
-            dataType: "json",
-            crossDomain: "true",
-            contentType: "application/json; charset=utf-8",
-            data: JSON.stringify(data),
-
-            success: function (e) {
-                // clear form and show a success message
-                alert(e);
-                document.getElementById("resizedForm").reset();
-                location.reload();
-            },
-            
-            error: function () {
-                // show an error message
-                alert("UnSuccessfull");
-        }});
-        
-/* 
-        var api = "https://hfdu6t80jj.execute-api.us-east-1.amazonaws.com/Dev/users"; // inserting the AWS url dev api
-
-        var params = {
-            "email": email,
-            "password": password
-        };
-
-        var xhttp = new XMLHttpRequest(); // open and handle the request to API Gateway
-        
-        xhttp.onreadystatechange = function(){
-            if(this.readyState == 4 && this.status == 200){
-                alert(xhttp.responseText);
-            }
-        };
-
-        xhttp.open("POST", api, false);
-
-        var response = xhttp.send(JSON.stringify(params));
-        console.log(response); */
     });
 
     $("#addUser").on("click", function () {
